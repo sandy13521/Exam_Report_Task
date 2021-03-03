6ce5cfaab353bc5c1cc045b3e1a6929f866b578e
sandy13521 <nandavarmukund1999@gmail.com>
2021-03-03 08:52:16 +0530

    Adding Documentation

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import db
 from flaskext.mysql import MySQL
 
 application = Flask(__name__)
+
+# application config
 application.config['MYSQL_DATABASE_USER'] = 'root'
 application.config['MYSQL_DATABASE_PASSWORD'] = ''
 application.config['MYSQL_DATABASE_DB'] = 'exam_report_task'
 application.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
+# MySql
 mysql = MySQL()
 mysql.init_app(application)
-
+# MySql Connection
 conn = mysql.connect()
 cursor = conn.cursor()
+
 
 @application.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
-@application.route('/entermarks', methods=['GET','POST'])
+
+@application.route('/entermarks', methods=['GET', 'POST'])
 def redirect_to_enter_marks():
-    global cursor,conn
+    global cursor, conn
     if request.method == 'GET':
         return render_template('enter_marks.html')
     else:
@@ -31,19 +36,19 @@ def redirect_to_enter_marks():
         marks_chemistry = str(request.form['marks_in_chemistry'])
         total = str(request.form['total'])
         percentage = str(request.form['percentage'])
-        print(total,percentage)
-        db.insert_student_details(conn,cursor,[rollno,name,marks_physics,marks_maths,marks_chemistry,total,percentage])
+        print(total, percentage)
+        db.insert_student_details(conn, cursor,
+                                  [rollno, name, marks_physics, marks_maths, marks_chemistry, total, percentage])
         return render_template('enter_marks.html')
-
 
 
 @application.route('/leaderboard', methods=['GET'])
 def redirect_to_view_leaderboard():
     if request.method == 'GET':
-        result = db.get_leaderboard(conn,cursor)
+        result = db.get_leaderboard(conn, cursor)
         print(result)
-        return render_template('leaderboard.html',result=result)
+        return render_template('leaderboard.html', result=result)
 
 
 if __name__ == '__main__':
-    application.run(debug=True,use_reloader=False)
+    application.run()
--- db.py
+++ db.py
@@ -1,9 +1,12 @@
-def insert_student_details(conn,cursor,request):
+# method to add student details into the database
+def insert_student_details(conn, cursor, request):
     sql_query = "insert into students values (%s,%s,%s,%s,%s,%s,%s)"
-    cursor.execute(sql_query,tuple(request))
+    cursor.execute(sql_query, tuple(request))
     conn.commit()
 
-def get_leaderboard(conn,cursor):
+
+# method to fetch data from database ordered by percentage
+def get_leaderboard(conn, cursor):
     sql_query = "select * from students order by percentage DESC"
     cursor.execute(sql_query)
     result = cursor.fetchall()
